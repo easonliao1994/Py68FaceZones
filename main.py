ce310105187481e9f57a820d96ae153078e78a1a
Eason <eason.liao.1994@gmail.com>
2023-11-18 16:38:32 +0800

    Add landmarks to image

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import dlib
- 
+import cv2
+
 img = dlib.load_rgb_image("res/lena.jpg")
+img_cv = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)  # Convert to OpenCV format
  
 detector = dlib.get_frontal_face_detector()
 faces = detector(img)
@@ -13,5 +15,8 @@ for face in faces:
     print(f'landmarks: {landmarks.num_parts}')
     print(f'{landmarks.parts()}')
  
-    # for part in landmarks.parts():
-        # Todo draw points into image
+    for i in range(landmarks.num_parts):
+        point = landmarks.part(i)
+        cv2.circle(img_cv, (point.x, point.y), 2, (0, 0, 255), -1)  # Draw red dot
+
+cv2.imwrite("res/lena_landmarks.jpg", img_cv)  # Save the image
